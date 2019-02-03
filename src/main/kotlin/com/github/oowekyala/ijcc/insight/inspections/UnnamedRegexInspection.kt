3467086d23afc5759f8631a2c81dc00396aafa1a
Clément Fournier <clement.fournier76@gmail.com>
2019-02-03 22:34:04 +0100

    Improve unnamed regex inspection

--- src/main/kotlin/com/github/oowekyala/ijcc/insight/inspections/UnnamedRegexInspection.kt
+++ src/main/kotlin/com/github/oowekyala/ijcc/insight/inspections/UnnamedRegexInspection.kt
@@ -1,10 +1,9 @@
 package com.github.oowekyala.ijcc.insight.inspections
 
 import com.github.oowekyala.ijcc.insight.model.RegexKind
-import com.github.oowekyala.ijcc.lang.psi.JccInlineRegularExpression
-import com.github.oowekyala.ijcc.lang.psi.JccNamedRegularExpression
-import com.github.oowekyala.ijcc.lang.psi.JccRegexprSpec
-import com.github.oowekyala.ijcc.lang.psi.JccVisitor
+import com.github.oowekyala.ijcc.insight.refactoring.AddNameToRegexIntention
+import com.github.oowekyala.ijcc.lang.psi.*
+import com.intellij.codeInspection.IntentionWrapper
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
 import org.intellij.lang.annotations.Language
@@ -18,22 +17,31 @@ class UnnamedRegexInspection : JccInspectionBase(DisplayName) {
     @Language("HTML")
     override fun getStaticDescription(): String = """
         Reports unnamed tokens and regex specs. Naming tokens <b>improves
-        error messages</b>. This only reports token specs of the TOKEN kind,
-        and regular expressions found in BNF expansions.
+        error messages</b>. Additionally, JavaCC ignores regex specs that
+        just reference another token.
     """.trimIndent()
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor = object : JccVisitor() {
         override fun visitInlineRegularExpression(o: JccInlineRegularExpression) {
-            if (o.parent !is JccRegexprSpec) {
-                holder.registerProblem(o, "Unnamed token")
+            if (o.parent !is JccRegexprSpec && o.regexpElement !is JccTokenReferenceUnit && o.regexpElement !is JccLiteralRegexpUnit) {
+                holder.registerProblem(o, GenericProblemDesc)
             }
         }
 
         override fun visitRegexprSpec(o: JccRegexprSpec) {
             if (o.regexKind == RegexKind.TOKEN) {
                 val regex = o.regularExpression
+
                 if (regex !is JccNamedRegularExpression) {
-                    holder.registerProblem(o, "Unnamed token")
+                    val desc = when (regex) {
+                        is JccRegularExpressionReference -> FreeStandingReferenceProblemDesc
+                        else                             -> GenericProblemDesc
+                    }
+                    holder.registerProblem(
+                        o,
+                        desc,
+                        IntentionWrapper.wrapToQuickFix(AddNameToRegexIntention(), o.containingFile)
+                    )
                 }
             }
         }
@@ -41,5 +49,8 @@ class UnnamedRegexInspection : JccInspectionBase(DisplayName) {
 
     companion object {
         const val DisplayName = "Unnamed regular expression"
+        const val GenericProblemDesc = "Unnamed token"
+        const val FreeStandingReferenceProblemDesc =
+                "JavaCC ignores free-standing regular expression references unless they have a different name"
     }
 }
---
+++ src/test/kotlin/com/github/oowekyala/ijcc/insight/inspections/UnnamedRegexInspectionTest.kt
@@ -0,0 +1,39 @@
+package com.github.oowekyala.ijcc.insight.inspections
+
+/**
+ * @author Clément Fournier
+ * @since 1.0
+ */
+class UnnamedRegexInspectionTest : JccInspectionTestBase(UnnamedRegexInspection()) {
+
+
+    private fun generic(s: String) = warningAnnot(s, UnnamedRegexInspection.GenericProblemDesc)
+    private fun reference(s: String) = warningAnnot(s, UnnamedRegexInspection.FreeStandingReferenceProblemDesc)
+
+    fun testLiteralString() = checkByText(
+        """< "foo" >""".inExpansionCtx()
+    )
+
+    fun testAll() = checkByText(
+        """
+            $DummyHeader
+
+            TOKEN: {
+                < FOO: "foo" >
+              | ${generic("< \" Foo \" >")}
+              | ${reference("< FOO >")}
+            }
+
+            void Foo() :{}{
+              < <FOO> > < "foobar" >
+            }
+
+        """.trimIndent()
+    )
+
+    fun testParen() = checkByText(
+        generic("""< ("foo") >""").inExpansionCtx()
+    )
+
+
+}
