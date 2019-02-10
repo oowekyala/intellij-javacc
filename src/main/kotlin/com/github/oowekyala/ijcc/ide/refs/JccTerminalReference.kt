oowekyala/intellij-javacc

commit 61d4d0a005dc455dcaa5088d2fc204406248d1f0

Make TerminalReference not use the grammar
This is done to avoid an infinite loop

src/main/kotlin/com/github/oowekyala/ijcc/ide/refs/JccTerminalReference.kt
@@ -1,6 +1,8 @@
 package com.github.oowekyala.ijcc.ide.refs
 
 import com.github.oowekyala.ijcc.ide.structureview.getPresentationIcon
+import com.github.oowekyala.ijcc.lang.model.ExplicitToken
+import com.github.oowekyala.ijcc.lang.model.SyntheticToken
 import com.github.oowekyala.ijcc.lang.model.Token
 import com.github.oowekyala.ijcc.lang.psi.*
 import com.github.oowekyala.ijcc.lang.psi.manipulators.JccIdentifierManipulator
@@ -27,7 +29,33 @@ class JccTerminalReference(referenceUnit: JccTokenReferenceRegexUnit) :
 
     fun resolveToken(): Token? {
         val searchedName = element.name ?: return null
-        return element.containingFile.lexicalGrammar.allTokens.firstOrNull { it.name == searchedName }
+
+        // This can't use the lexical grammar directly,
+        // because the lexical grammar uses references of this type
+        // to resolve string tokens
+
+        return element.containingFile
+            .grammarFileRoot.allProductions()
+            .flatMap { it.tokensUnfiltered() }
+            .firstOrNull { it.name == searchedName }
+    }
+
+    private fun JccGrammarFileRoot?.allProductions(): Sequence<JccProduction> =
+            this?.childrenSequence()?.filterIsInstance<JccProduction>().orEmpty()
+
+    private fun JccProduction.tokensUnfiltered(): Sequence<Token> {
+        return when (this) {
+            is JccRegexProduction -> regexSpecList.asSequence().map(::ExplicitToken)
+
+            is JccBnfProduction   ->
+                expansion
+                    ?.descendantSequence(includeSelf = true)
+                    ?.filterIsInstance<JccRegexExpansionUnit>()
+                    .orEmpty()
+                    .map(::SyntheticToken)
+
+            else                  -> emptySequence()
+        }
     }
 
     override fun getVariants(): Array<Any> =

src/main/kotlin/com/github/oowekyala/ijcc/lang/model/LexicalGrammar.kt
@@ -13,6 +13,85 @@ import com.github.oowekyala.ijcc.util.runIt
  */
 class LexicalGrammar(grammarFileRoot: JccGrammarFileRoot?) {
 
+    // TODO optimisations that come to mind:
+    // map of name to token, to resolve references!!
+    // map of string to token, to resolve string tokens
+
+    init {
+
+        val onlyRef = mutableSetOf<Token>()
+        val onlyString = mutableSetOf<Token>()
+        val otherTokens = mutableSetOf<Token>()
+
+        val nameToTokenMap = mutableMapOf<String, Token>()
+        val indirectionMap = mutableMapOf<Token, String>()
+        val tokenToStates = mutableMapOf<Token, List<String>>()
+
+        // may contain tokens that JavaCC ignores, named tokens are never ignored
+        val tokenToNameMap =
+                grammarFileRoot.allProductions()
+                    .flatMap { it.tokensUnfiltered() }
+                    .associateWith { it.name }
+
+        tokenToNameMap.forEach { (token, tokenName) ->
+
+            val unit = token.regularExpression?.getRootRegexElement(followReferences = false)
+
+            var refName: String? = null
+            val lst = when (unit) {
+                null                          -> null
+                is JccLiteralRegexUnit        -> onlyString
+                is JccTokenReferenceRegexUnit -> {
+                    refName = unit.name
+                    onlyRef
+                }
+                else                          -> otherTokens
+            }
+
+            lst?.add(token)
+
+            // named tokens won't be removed. There may be duplicates, they're JavaCC errors anyway
+            tokenName?.let { nameToTokenMap[it] = token }
+            refName?.runIt { indirectionMap[token] = it }
+            tokenToStates[token] = token.lexicalStatesOrEmptyForAll
+
+        }
+
+        val visited = mutableSetOf<Token>()
+        /*
+            Here we add check for tokens that are only references
+            but reference (possibly indirectly) a string token. Those
+            all are string tokens.
+         */
+        for (refToken in onlyRef) {
+
+            if (refToken in visited) continue
+            visited += refToken
+
+            val indirectionPath = mutableListOf<Token>()
+
+            var resolved: Token? = indirectionMap[refToken]?.let { nameToTokenMap[it] }
+
+            while (
+                resolved != null
+                && indirectionMap.containsKey(resolved)
+                && resolved !in indirectionPath  // avoid infinite loop caused by cyclic references
+            ) {
+                indirectionPath += resolved
+                resolved = indirectionMap[resolved]?.let { nameToTokenMap[it] }
+            }
+
+            if (indirectionPath.isNotEmpty() && resolved in onlyString) {
+                // then all the path are string tokens
+                onlyString.addAll(indirectionPath)
+                visited.addAll(indirectionPath)
+            }
+        }
+
+        // Then we prune all string tokens that are duplicates
+
+
+    }
 
     /** All the defined lexical states. */
     private val lexicalStatesMap: Map<String, LexicalState> by lazy {
@@ -21,7 +100,8 @@ class LexicalGrammar(grammarFileRoot: JccGrammarFileRoot?) {
         }
     }
 
-    /** All the tokens. TODO optimise s.t. it's not necessary to partition them by state. */
+
+    /** All the tokens. */
     val allTokens: Sequence<Token> by lazy {
         lexicalStates.flatMap { it.tokens }.asSequence()
     }
@@ -39,7 +119,27 @@ class LexicalGrammar(grammarFileRoot: JccGrammarFileRoot?) {
     val defaultState: LexicalState
         get() = lexicalStatesMap.getValue(LexicalState.DefaultStateName)
 
+
     companion object {
+
+        private fun JccGrammarFileRoot?.allProductions(): Sequence<JccProduction> =
+                this?.childrenSequence()?.filterIsInstance<JccProduction>().orEmpty()
+
+        private fun JccProduction.tokensUnfiltered(): Sequence<Token> {
+            return when (this) {
+                is JccRegexProduction -> regexSpecList.asSequence().map(::ExplicitToken)
+
+                is JccBnfProduction   ->
+                    expansion
+                        ?.descendantSequence(includeSelf = true)
+                        ?.filterIsInstance<JccRegexExpansionUnit>()
+                        .orEmpty()
+                        .map(::SyntheticToken)
+
+                else                  -> emptySequence()
+            }
+        }
+
 
         private fun buildStatesMap(allProductions: () -> Sequence<JccProduction>): Map<String, LexicalState> {
 
@@ -94,7 +194,8 @@ class LexicalGrammar(grammarFileRoot: JccGrammarFileRoot?) {
                             val token = when (val r = regexExpansion.regularExpression.getRootRegexElement(false)) {
 
                                 is JccLiteralRegexUnit        ->  // if the string isn't covered by an explicit token, it's synthesized
-                                    currentSpecs.firstOrNull { it.matchesLiteral(r) } ?: SyntheticToken(regexExpansion)
+                                    currentSpecs.firstOrNull { it.matchesLiteral(r, followReferences = false) } // following references may cause infinite recursion
+                                        ?: SyntheticToken(regexExpansion)
 
                                 // necessarily references an explicit token
                                 is JccTokenReferenceRegexUnit -> null

src/main/kotlin/com/github/oowekyala/ijcc/JavaccFoldingBuilder.kt
@@ -75,7 +75,7 @@ class JavaccFoldingBuilder : CustomFoldingBuilder() {
 
 
         private fun literalRegexForRef(regexRef: JccTokenReferenceRegexUnit): JccLiteralRegexUnit? =
-                regexRef.typedReference.resolveToken()?.asStringToken
+                regexRef.typedReference.resolveToken()?.getAsStringToken()
 
         private val newLines = CharArray(2)
 

src/main/kotlin/com/github/oowekyala/ijcc/ide/inspections/DuplicateImplicitStringTokenDefinitionInspection.kt
@@ -27,7 +27,7 @@ class DuplicateImplicitStringTokenDefinitionInspection : JccInspectionBase(Displ
         file.lexicalGrammar
             .allTokens
             .filterIsInstance<SyntheticToken>()
-            .filter { it.asStringToken != null }
+            .filter { it.getAsStringToken() != null }
             .forEach {
 
             }

src/main/kotlin/com/github/oowekyala/ijcc/ide/intentions/LiteralVsReferenceIntentions.kt
@@ -17,13 +17,13 @@ class TokenInliningIntention
 ) {
 
     override fun isApplicableTo(element: JccTokenReferenceRegexUnit): Boolean =
-            element.typedReference.resolveToken()?.asStringToken != null
+            element.typedReference.resolveToken()?.getAsStringToken() != null
 
     override fun run(project: Project, editor: Editor, element: JccTokenReferenceRegexUnit): () -> Unit {
         val newLiteral: JccLiteralRegexUnit =
                 element.typedReference
                     .resolveToken()!!
-                    .asStringToken!!
+                    .getAsStringToken()!!
                     .text
                     .let { createRegexElement(project, it) }
 

src/main/kotlin/com/github/oowekyala/ijcc/ide/quickdoc/JccTerminalDocMaker.kt
@@ -86,7 +86,7 @@ object JccTerminalDocMaker {
             val reffed = o.typedReference.resolveToken()
 
             // make the linktext be the literal if needed.
-            val linkText = reffed?.asStringToken?.text ?: angles(o.name!!)
+            val linkText = reffed?.getAsStringToken()?.text ?: angles(o.name!!)
 
             DocumentationManager.createHyperlink(
                 sb,

src/main/kotlin/com/github/oowekyala/ijcc/ide/refs/JccBnfStringLiteralReference.kt
@@ -51,7 +51,7 @@ class JccBnfStringLiteralReference(element: JccLiteralRegexUnit) :
         .tokens
         .filter { !it.isPrivate }
         .mapNotNull { token ->
-            val asString = token.asStringToken ?: return@mapNotNull null
+            val asString = token.getAsStringToken() ?: return@mapNotNull null
 
             LookupElementBuilder
                 .create(asString.text)

src/main/kotlin/com/github/oowekyala/ijcc/lang/model/Token.kt
@@ -51,8 +51,8 @@ sealed class Token {    // we could have a type parameter here, but I'm too lazy
     val line: Int? get() = psiElement?.lineNumber
 
     /** Returns true if this is a single literal token. */
-    val asStringToken: JccLiteralRegexUnit?
-        get() = regularExpression?.asSingleLiteral(followReferences = false)
+    fun getAsStringToken(followReferences: Boolean = false): JccLiteralRegexUnit? =
+            regularExpression?.asSingleLiteral(followReferences)
 
     val psiElement: JccPsiElement? get () = psiPointer.element
 
@@ -69,17 +69,14 @@ sealed class Token {    // we could have a type parameter here, but I'm too lazy
      * Returns true if this token is the same literal unit as this one,
      * modulo [isIgnoreCase].
      */
-    fun matchesLiteral(unit: JccLiteralRegexUnit): Boolean =
-            asStringToken?.let {
-                unit.match.equals(it.match, ignoreCase = isIgnoreCase)
-            } == true
+    fun matchesLiteral(unit: JccLiteralRegexUnit, followReferences: Boolean = false): Boolean = matchesLiteral(unit.match, followReferences)
 
     /**
      * Returns true if this token is the same literal unit as this one,
      * modulo [isIgnoreCase].
      */
-    fun matchesLiteral(literalMatch: String): Boolean =
-            asStringToken?.let {
+    fun matchesLiteral(literalMatch: String, followReferences: Boolean = false): Boolean =
+            getAsStringToken(followReferences)?.let {
                 literalMatch.equals(it.match, ignoreCase = isIgnoreCase)
             } == true
 
@@ -94,10 +91,12 @@ sealed class Token {    // we could have a type parameter here, but I'm too lazy
 
             if (t1.name != null && t2.name != null && t2.name != t1.name) return false
 
-            val t1Str = t1.asStringToken
-            val t2Str = t2.asStringToken
+            val t1Str = t1.getAsStringToken()
+            val t2Str = t2.getAsStringToken()
 
-            return t1Str != null && t2Str != null && (t1.matchesLiteral(t2Str) || t2.matchesLiteral(t1Str))
+            return t1Str != null
+                    && t2Str != null
+                    && (t1.matchesLiteral(t2Str) || t2.matchesLiteral(t1Str))
         }
 
         /**

commit e71558c3f997b95713543e3a83f0a37dfe4b8733

Optimise JccTerminalReference
This fixes a major performance problem that left the IDE hanging when
opening a file that had many token references (eg PlDocAst). That
problem was introduced in 61d4d0a

src/main/kotlin/com/github/oowekyala/ijcc/ide/refs/JccTerminalReference.kt
@@ -1,8 +1,7 @@
 package com.github.oowekyala.ijcc.ide.refs
 
 import com.github.oowekyala.ijcc.ide.structureview.getPresentationIcon
-import com.github.oowekyala.ijcc.lang.model.ExplicitToken
-import com.github.oowekyala.ijcc.lang.model.SyntheticToken
+import com.github.oowekyala.ijcc.lang.model.LexicalGrammar
 import com.github.oowekyala.ijcc.lang.model.Token
 import com.github.oowekyala.ijcc.lang.psi.*
 import com.github.oowekyala.ijcc.lang.psi.manipulators.JccIdentifierManipulator
@@ -14,7 +13,17 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReferenceBase
 
 /**
- * Reference to a named [Token].
+ * Reference to a named [Token]. Used internally by [LexicalGrammar]
+ * when building lexical states so this implementation *cannot use
+ * lexical states directly*!! Otherwise we fall into bottomless recursion.
+ * But since all named tokens are preserved (no filtering by superseding
+ * match is needed like for string tokens), [LexicalGrammar] stores an
+ * index of tokens by their name, which is eagerly built on initialisation,
+ * and allows this reference to resolve its result very fast.
+ *
+ * This is a *major* performance optimisation that alone lets the plugin
+ * open files like PlDocAst.jjt quickly instead of hanging. Compare 61d4d0a
+ * with 
  *
  * @author Clément Fournier
  * @since 1.0
@@ -30,35 +39,7 @@ class JccTerminalReference(referenceUnit: JccTokenReferenceRegexUnit) :
     fun resolveToken(): Token? {
         val searchedName = element.name ?: return null
 
-        // This can't use the lexical grammar directly,
-        // because the lexical grammar uses references of this type
-        // to resolve string tokens
-        // TODO you should optimise that, split lexical grammar initialisation in 2
-
-        return element.containingFile
-            .grammarFileRoot.allProductions()
-            .flatMap { it.tokensUnfiltered() }
-            // references can't declare themselves
-            .filter { it.regularExpression !is JccRefRegularExpression }
-            .firstOrNull { it.name == searchedName }
-    }
-
-    private fun JccGrammarFileRoot?.allProductions(): Sequence<JccProduction> =
-        this?.childrenSequence()?.filterIsInstance<JccProduction>().orEmpty()
-
-    private fun JccProduction.tokensUnfiltered(): Sequence<Token> {
-        return when (this) {
-            is JccRegexProduction -> regexSpecList.asSequence().map(::ExplicitToken)
-
-            is JccBnfProduction   ->
-                expansion
-                    ?.descendantSequence(includeSelf = true)
-                    ?.filterIsInstance<JccRegexExpansionUnit>()
-                    .orEmpty()
-                    .map(::SyntheticToken)
-
-            else                  -> emptySequence()
-        }
+        return element.containingFile.lexicalGrammar.getTokenByName(searchedName)
     }
 
     override fun getVariants(): Array<Any> =

src/main/kotlin/com/github/oowekyala/ijcc/lang/model/LexicalGrammar.kt
@@ -13,6 +13,19 @@ import com.github.oowekyala.ijcc.util.runIt
  */
 class LexicalGrammar(grammarFileRoot: JccGrammarFileRoot?) {
 
+    private val namedTokensMap: Map<String, Token>
+
+    init {
+
+        namedTokensMap = grammarFileRoot.allProductions()
+            .flatMap { it.tokensUnfiltered() }
+            // references can't declare themselves
+            .filter { it.regularExpression is JccNamedRegularExpression }
+            .associateBy { it.name!! }
+
+    }
+
+
     /** All the defined lexical states. */
     private val lexicalStatesMap: Map<String, LexicalState> by lazy {
         buildStatesMap {
@@ -27,6 +40,8 @@ class LexicalGrammar(grammarFileRoot: JccGrammarFileRoot?) {
     }
 
     val lexicalStates: Collection<LexicalState> = lexicalStatesMap.values
+
+    fun getTokenByName(name: String): Token? = namedTokensMap[name]
 
     fun getLexicalState(name: String): LexicalState? = lexicalStatesMap[name]
 
@@ -116,6 +131,24 @@ class LexicalGrammar(grammarFileRoot: JccGrammarFileRoot?) {
             }
 
             return builders.mapValues { (_, v) -> v.build() }
+        }
+
+        private fun JccGrammarFileRoot?.allProductions(): Sequence<JccProduction> =
+            this?.childrenSequence()?.filterIsInstance<JccProduction>().orEmpty()
+
+        private fun JccProduction.tokensUnfiltered(): Sequence<Token> {
+            return when (this) {
+                is JccRegexProduction -> regexSpecList.asSequence().map(::ExplicitToken)
+
+                is JccBnfProduction   ->
+                    expansion
+                        ?.descendantSequence(includeSelf = true)
+                        ?.filterIsInstance<JccRegexExpansionUnit>()
+                        .orEmpty()
+                        .map(::SyntheticToken)
+
+                else                  -> emptySequence()
+            }
         }
 
 