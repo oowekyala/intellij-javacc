9d804dffc1224d0b8385c8f258fcf86956a3b48b
Clément Fournier <clement.fournier76@gmail.com>
2019-02-10 03:02:39 +0100

    Make delete expansion intention accessible

--- src/main/kotlin/com/github/oowekyala/ijcc/ide/intentions/DeleteExpansionIntention.kt
+++ src/main/kotlin/com/github/oowekyala/ijcc/ide/intentions/DeleteExpansionIntention.kt
@@ -1,6 +1,8 @@
 package com.github.oowekyala.ijcc.ide.intentions
 
+import com.github.oowekyala.ijcc.lang.psi.JccBnfProduction
 import com.github.oowekyala.ijcc.lang.psi.JccExpansion
+import com.intellij.codeInsight.intention.LowPriorityAction
 import com.intellij.codeInspection.IntentionWrapper
 import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.openapi.editor.Editor
@@ -14,11 +16,11 @@ import com.intellij.psi.PsiFile
  * @since 1.1
  */
 class DeleteExpansionIntention(val name: String = "Delete expansion")
-    : SelfTargetingOffsetIndependentIntention<JccExpansion>(JccExpansion::class.java, name) {
+    : SelfTargetingOffsetIndependentIntention<JccExpansion>(JccExpansion::class.java, name), LowPriorityAction {
 
     override fun applyTo(project: Project, editor: Editor?, element: JccExpansion) = element.delete()
 
-    override fun isApplicableTo(element: JccExpansion): Boolean = true
+    override fun isApplicableTo(element: JccExpansion): Boolean = element.parent !is JccBnfProduction
 
     companion object {
         fun quickFix(name: String = "Delete expansion", file: PsiFile): LocalQuickFix =
