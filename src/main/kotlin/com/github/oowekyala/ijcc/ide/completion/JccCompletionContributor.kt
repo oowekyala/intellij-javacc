0adcfa9f9a5c6be52be01e3b30a9e868d5b24346
Clément Fournier <clement.fournier76@gmail.com>
2019-02-11 19:08:15 +0100

    Refine regex prod suggestion

--- src/main/kotlin/com/github/oowekyala/ijcc/ide/completion/JccCompletionContributor.kt
+++ src/main/kotlin/com/github/oowekyala/ijcc/ide/completion/JccCompletionContributor.kt
@@ -1,14 +1,12 @@
 package com.github.oowekyala.ijcc.ide.completion
 
 import com.github.oowekyala.ijcc.lang.JccTypes
-import com.github.oowekyala.ijcc.lang.JccTypes.JCC_IDENT
 import com.github.oowekyala.ijcc.lang.model.GrammarOptions
 import com.github.oowekyala.ijcc.lang.model.JccOptionType.BaseOptionType.BOOLEAN
 import com.github.oowekyala.ijcc.lang.model.RegexKind
 import com.github.oowekyala.ijcc.lang.psi.*
 import com.intellij.codeInsight.TailType
 import com.intellij.codeInsight.completion.*
-import com.intellij.codeInsight.completion.CompletionInitializationContext.START_OFFSET
 import com.intellij.codeInsight.completion.impl.CamelHumpMatcher
 import com.intellij.codeInsight.completion.simple.BracesTailType
 import com.intellij.codeInsight.lookup.LookupElementBuilder
@@ -16,6 +14,7 @@ import com.intellij.codeInsight.lookup.TailTypeDecorator
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.patterns.PsiElementPattern
+import com.intellij.patterns.StandardPatterns
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
@@ -36,7 +35,7 @@ class JccCompletionContributor : CompletionContributor() {
             .inFile(PlatformPatterns.instanceOf(JccFile::class.java))
             .andNot(psiElement().inside(PsiComment::class.java))
 
-
+        StandardPatterns.string()
         val optionValuePattern =
             psiElement().withAncestor(2, psiElement(JccOptionBinding::class.java))
                 .afterSibling(
@@ -90,23 +89,20 @@ class JccCompletionContributor : CompletionContributor() {
                     JccGrammarFileRoot::class.java,
                     JccTokenManagerDecls::class.java,
                     JccRegexProduction::class.java,
+                    JccOptionSection::class.java,
                     JccFile::class.java
                 )
 
-                when (parent) {
-                    is JccGrammarFileRoot, is JccFile -> {
-                        // topLevel
-
-                        for (regexKind in RegexKind.values()) {
-                            result.addElement(
-                                TailTypeDecorator.withTail(
-                                    LookupElementBuilder.create(regexKind.name + " : ").withBoldness(true),
-                                    BracesTailType()
-                                )
-                            )
-                        }
+                val accepted =
+                    when (parent) {
+                        // could be an unclosed one
+                        is JccNonTerminalProduction -> parent.javaBlock == null
+                        // root
+                        is JccGrammarFileRoot, is JccFile -> true
+                        else -> false
                     }
-                }
+
+                if (accepted) result.addAllElements(RegexProdVariants)
             }
         })
     }
@@ -114,26 +110,35 @@ class JccCompletionContributor : CompletionContributor() {
     override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
         super.fillCompletionVariants(parameters, result)
     }
-//
-//    override fun duringCompletion(context: CompletionInitializationContext) {
-//        val psiFile = context.file as? JccFile ?: return
-//
-//        val element = psiFile.findElementAt(context.startOffset)
-//        if (element?.isOfType(JCC_IDENT) == true) {
-//            context.dummyIdentifier = "x"
-//            context.offsetMap.addOffset(START_OFFSET, element.textOffset-1)// = element.textOffset - 1
-//        }
-//    }
+    //
+    //    override fun duringCompletion(context: CompletionInitializationContext) {
+    //        val psiFile = context.file as? JccFile ?: return
+    //
+    //        val element = psiFile.findElementAt(context.startOffset)
+    //        if (element?.isOfType(JCC_IDENT) == true) {
+    //            context.dummyIdentifier = "x"
+    //            context.offsetMap.addOffset(START_OFFSET, element.textOffset-1)// = element.textOffset - 1
+    //        }
+    //    }
 
 
     companion object {
+
+        val RegexProdVariants: List<TailTypeDecorator<LookupElementBuilder>> =
+            RegexKind.values().map {
+                LookupElementBuilder.create(it.name + " : ")
+                    .withBoldness(true)
+                    .withTail(BracesTailType())
+            }
+
 
         val OptionVariants: List<TailTypeDecorator<LookupElementBuilder>> =
             GrammarOptions.knownOptions.map { (name, opt) ->
                 LookupElementBuilder.create(name)
                     .withBoldness(true)
                     .withTypeText("(${opt.expectedType}) = ${opt.staticDefaultValue.presentable()}", true)
-            }.map { TailTypeDecorator.withTail(it, TailType.EQ) }
+                    .withTail(TailType.EQ)
+            }
 
         private fun Any?.presentable(): String = when (this) {
             is String -> "\"${this}\""
--- src/main/kotlin/com/github/oowekyala/ijcc/ide/completion/LookupUtil.kt
+++ src/main/kotlin/com/github/oowekyala/ijcc/ide/completion/LookupUtil.kt
@@ -1,8 +1,12 @@
 package com.github.oowekyala.ijcc.ide.completion
 
+import com.intellij.codeInsight.TailType
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.codeInsight.lookup.TailTypeDecorator
 
 
 fun LookupElementBuilder.withTail(tail: String) =
     TailTypeDecorator.withTail(this, MultiCharTailType(tail))
+
+fun LookupElementBuilder.withTail(tailType: TailType) =
+    TailTypeDecorator.withTail(this, tailType)
