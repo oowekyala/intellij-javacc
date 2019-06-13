37b869be8d6e1b71a06fecf7d99eefcac58734b6
Clément Fournier <clement.fournier76@gmail.com>
2019-06-13 14:45:51 +0200

    Cleanup

--- jjtricks/src/main/kotlin/com/github/oowekyala/jjtx/postprocessor/JavaccClassMapper.kt
+++ jjtricks/src/main/kotlin/com/github/oowekyala/jjtx/postprocessor/JavaccClassMapper.kt
@@ -1,23 +1,20 @@
 package com.github.oowekyala.jjtx.postprocessor
 
 import com.github.oowekyala.ijcc.lang.model.addParserPackage
-import com.github.oowekyala.ijcc.lang.model.parserQualifiedName
 import com.github.oowekyala.jjtx.JjtxContext
-import com.github.oowekyala.jjtx.reporting.reportFatal
 import spoon.OutputType
 import spoon.processing.AbstractProcessor
 import spoon.processing.Processor
 import spoon.reflect.code.CtBlock
 import spoon.reflect.code.CtIf
 import spoon.reflect.code.CtLiteral
-import spoon.reflect.code.CtTry
-import spoon.reflect.declaration.CtType
+import spoon.reflect.code.CtStatement
+import spoon.reflect.declaration.CtElement
 import spoon.reflect.factory.TypeFactory
 import spoon.reflect.reference.CtTypeReference
 import spoon.reflect.visitor.filter.TypeFilter
 import java.nio.file.Path
 import spoon.Launcher as SpoonLauncher
-import spoon.reflect.code.CtStatement as CtStatement
 
 
 fun mapJavaccOutput(ctx: JjtxContext, jccOutput: Path, realOutput: Path, otherSources: List<Path>) {
@@ -46,53 +43,21 @@ fun mapJavaccOutput(ctx: JjtxContext, jccOutput: Path, realOutput: Path, otherSo
         Pair(this, model)
     }
 
-    val realToken = ctx.tokenClass
-
-    val realTokenRef = TypeFactory().createReference<Any>(realToken.qualifiedName)
 
     val jjToken = ctx.jjtxOptsModel.addParserPackage("Token")
-    val renamer: Processor<CtTypeReference<*>> = object : AbstractProcessor<CtTypeReference<*>>() {
-        override fun process(element: CtTypeReference<*>) {
-            if (element.qualifiedName == jjToken) {
-                element.replace(realTokenRef.clone())
-            }
-        }
-    }
+    val renamer: Processor<CtTypeReference<*>> = TypeReferenceRenamer(jjToken, ctx.tokenClass.qualifiedName)
 
-    val inliner = object : AbstractProcessor<CtIf>() {
-        override fun process(element: CtIf) {
-            val condition = element.condition as? CtLiteral<Boolean> ?: return
 
-            if (condition.value == true) {
-                element.replace(element.getThenStatement<CtStatement>().clone())
-            } else if (condition.value == false) {
-                element.replace(element.getElseStatement<CtStatement>().clone())
-            }
-        }
-    }
+    val processors = listOf(
+        // can't factorise that bc of the inline
+        renamer.treeProcessor(),
+        IfStmtConstantFolder.treeProcessor(),
+        BlockUnwrapper.treeProcessor()
+    )
 
-    val blockFlattener = object : AbstractProcessor<CtBlock<*>>() {
-        override fun process(element: CtBlock<*>) {
-
-            if (element.statements.size == 1 && element.parent is CtBlock<*>) {
-                // element.setImplicit<CtBlock<*>>(true)
-                element.replace(element.statements.first().clone())
-            }
-        }
-    }
 
     spoonModel.allTypes.forEach {
-        it.getElements(TypeFilter(CtTypeReference::class.java))?.forEach {
-            renamer.process(it)
-        }
-        it.getElements(TypeFilter(CtIf::class.java))?.forEach {
-            inliner.process(it)
-        }
-
-        it.getElements(TypeFilter(CtBlock::class.java))?.forEach {
-            blockFlattener.process(it)
-        }
-
+        processors.forEach { p -> p.process(it) }
         spoon.createOutputWriter().createJavaFile(it)
     }
 
@@ -100,6 +65,51 @@ fun mapJavaccOutput(ctx: JjtxContext, jccOutput: Path, realOutput: Path, otherSo
 
 
 
+}
+
+inline fun <reified T : CtElement> Processor<T>.treeProcessor(): Processor<in CtElement> =
+    object : AbstractProcessor<CtElement>() {
+        override fun process(element: CtElement) {
+            element.getElements(TypeFilter(T::class.java)).forEach {
+                this@treeProcessor.process(it)
+            }
+        }
+    }
+
+object BlockUnwrapper : AbstractProcessor<CtBlock<*>>() {
+    override fun process(element: CtBlock<*>) {
+
+        if (element.statements.size == 1 && element.parent is CtBlock<*>) {
+            // element.setImplicit<CtBlock<*>>(true)
+            element.replace(element.statements.first().clone())
+        }
+    }
+}
+
+object IfStmtConstantFolder : AbstractProcessor<CtIf>() {
+    override fun process(element: CtIf) {
+        val condition = element.condition as? CtLiteral<Boolean> ?: return
+
+        if (condition.value == true) {
+            element.replace(element.getThenStatement<CtStatement>().clone())
+        } else if (condition.value == false) {
+            element.replace(element.getElseStatement<CtStatement>().clone())
+        }
+    }
+}
+
+class TypeReferenceRenamer(private val sourceQname: String,
+                           targetQname: String)
+    : AbstractProcessor<CtTypeReference<*>>() {
+
+    private val targetRef = TypeFactory().createReference<Any>(targetQname)!!
+
+
+    override fun process(element: CtTypeReference<*>) {
+        if (element.qualifiedName == sourceQname) {
+            element.replace(targetRef.clone())
+        }
+    }
 }
 
 
