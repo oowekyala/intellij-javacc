8e4ef5e37aabe90b14045f5da1b2caf4c0aed335
Clément Fournier <clement.fournier76@gmail.com>
2020-09-19 01:08:04 +0200

    TODO jcc 21 parser test

--- src/test/kotlin/com/github/oowekyala/ijcc/lang/parser/Jcc21ParserTests.kt
+++ src/test/kotlin/com/github/oowekyala/ijcc/lang/parser/Jcc21ParserTests.kt
@@ -6,12 +6,13 @@ import com.intellij.testFramework.ParsingTestCase
 
 /**
  * @author Clément Fournier
- * @since 1.0
+ * @since 1.6
  */
 class Jcc21ParserTests : ParsingTestCase("21", "javacc", JavaccParserDefinition) {
 
     private val checkIt = true
 
+    // TODO the end is wrong
     fun testSimple() = doTest(checkIt)
 
     override fun getTestDataPath(): String = ParserTestDataPath
